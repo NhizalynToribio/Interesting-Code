97de03b12c7e1bddc3ceafcb61c65bf571752456
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-11-19 17:49:55 +0800

    Start

---
+++ Interesting.py
@@ -0,0 +1,4 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("***************** BSCOE 2 -2 *******************")
+print("******** SEATWORK (EXCITING WEBSITE) ***********")
