d6c859b8d87c9e91101f6eeffbd8adfde1a9c65c
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-11-19 20:06:11 +0800

    Addidng references and Fixing some errors and structures

--- Clock - Toribio.py
+++ Clock - Toribio.py
@@ -1,7 +1,8 @@
 # PROGRAMMER: NHIZALYN P. TORIBIO
-# YEAR & AND SECTION: BSCOE 2 - 2
+# YEAR & SECTION: BSCOE 2 - 2
 # LABORATORY EXERCISE NO 4
 
+# This is the tkinter of the Clock
 from tkinter import *
 from tkinter.ttk import *
 from time import strftime
@@ -10,13 +11,18 @@ root = Tk()
 root.title("Clock")
 
 
+# This is the structure of the Clock
 def time():
     string = strftime('%H:%M:%S %p')
     label.config(text=string)
     label.after(1000, time)
 
 
-label = Label (root, font=("ds=digital", 80), background = "black", foreground = "cyan")
+label = Label (root, font=("ds=digital", 80), background="blue", foreground = "cyan")
 label.pack(anchor="center")
+
+# This is the output of the clock
 time()
 mainloop()
+
+# Use reference is : https://www.youtube.com/watch?v=at7rpdT8FeI
