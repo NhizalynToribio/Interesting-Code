acbcb960b87146b84827f9239becdc15a5efe5cd
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-11-19 20:21:37 +0800

    Done with the password process

--- Password Generator - Toribio.py
+++ Password Generator - Toribio.py
@@ -11,3 +11,16 @@ def Generate_Password(pwlength):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
 
     passwords = []
+
+    for i in pwlength:
+        password = ""
+        for j in range(i):
+            next_letter_index = random.randrange(len(alphabet))
+            password = password + alphabet[next_letter_index]
+
+        password = replaceWithNumber(password)
+        password = replaceWithUppercaseLetter(password)
+
+        passwords.append(password)
+
+    return passwords
