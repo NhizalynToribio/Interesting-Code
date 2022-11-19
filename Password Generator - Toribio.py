f4bb70372c1830abe86575fc2252038a59069ba2
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2022-11-19 20:10:59 +0800

    start of second codes which is Password Generator

---
+++ Password Generator - Toribio.py
@@ -0,0 +1,13 @@
+# PROGRAMMER: NHIZALYN P. TORIBIO
+# YEAR & SECTION: BSCOE 2 - 2
+# SECOND EXCITING CODES
+
+# This is importing a random file
+import random
+
+
+def Generate_Password(pwlength):
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    passwords = []
